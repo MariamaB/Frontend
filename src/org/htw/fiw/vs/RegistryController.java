ddaa6728d6b9ccb5fc2f3064003c94d548f01b21
Malwine <malwine@mailbox.org>
2017-02-08 14:46:50 +0100

    send data(name and pattern) via URL

--- src/org/htw/fiw/vs/RegistryController.java
+++ src/org/htw/fiw/vs/RegistryController.java
@@ -8,6 +8,8 @@ import java.rmi.RemoteException;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
 
 import org.htw.fiw.vs.team1.ControllerInterface;
 
@@ -16,15 +18,18 @@ import org.htw.fiw.vs.team1.ControllerInterface;
 public class RegistryController{
 	
 	private static String dropdown() {
-		return "<select><option value=\"muster1\">Muster1</option><option value=\"an-aus\">An Aus</option></select>";
+		return "<select name=\"pattern\"><option value=\"no-pattern\">No Pattern</option><option value=\"blink\">Blink</option></select>";
 	}
 	
 	@GET
-	public String sendRegistryContent() throws RemoteException, MalformedURLException, NotBoundException {
-
+	public String sendRegistryContent(@QueryParam("name") String name) throws RemoteException, MalformedURLException, NotBoundException {
+	
 		IBinder registry = (IBinder) Naming.lookup("rmi://localhost/binder");
 		String[] list = registry.list();
-		 
+		
+		ControllerInterface ci = (ControllerInterface) registry.lookup("controller");
+		ci.printFromServer(name);
+		
 		String registeredElement = printList(list);
 		return new MyHtml().getHtml(registeredElement);
 	}
@@ -33,9 +38,7 @@ public class RegistryController{
 	@POST
 	public String sendData() throws MalformedURLException, RemoteException, NotBoundException {
 		IBinder registry = (IBinder) Naming.lookup("rmi://localhost/binder");
-		ControllerInterface ci = (ControllerInterface) registry.lookup("controller");
-		ci.printFromServer("yes this is the server speaking");
-		
+
 		String[] list = registry.list();
 		String registeredElement = printList(list);
 		return new MyHtml().getHtml(registeredElement);
@@ -47,7 +50,7 @@ public class RegistryController{
 		 for (String elm : list) {
 			 System.out.println(elm);
 			 if (elm.contains("button")) {
-				 registeredElement = registeredElement + "<br/>"+ i + "." + elm + RegistryController.dropdown() + "<button type=\"submit\" name=\"" + elm + "\">Click</button>";
+				 registeredElement = registeredElement + "<br/>"+ i + "." + elm + RegistryController.dropdown() + "<button type=\"submit\" name=\"name\" value=\"" + elm +"\"" + "\">Click</button>";
 			 }
 			 else {
 				 registeredElement = registeredElement+"<br />"+i+"."+elm;
